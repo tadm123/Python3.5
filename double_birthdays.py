5c51c1a29c354d089c3d63196196a38cf326e11f
tadm123 <dramir19@fau.edu>
2016-11-26 00:53:29 -0500

    Update double_birthdays.py

--- double_birthdays.py
+++ double_birthdays.py
@@ -4,19 +4,16 @@
 # Input the number of students that you wish to sample.
 # This problem is based on the 'Birthday paradox' https://en.wikipedia.org/wiki/Birthday_problem
 
-
 import random
 
 NUM_OF_SAMPLES = 10000
 NUM_DAYS_IN_A_YEAR = 365
-
 
 def checking(my_dict):                  #returns 1 if the sample has double birthdays (or more), if not returns 0
       for i in my_dict.values():
               if i >= 2:
                       return 1
       return 0
-
 
 check = []      
 count = 0
@@ -30,15 +27,11 @@ for i in range(NUM_OF_SAMPLES):
               else:
                       my_dict[num] = my_dict[num] + 1
       check.append(checking(my_dict))
-
       
 for i in check:                 #
 	if i == 1:
-		count+=1      
-        
-
+		count+=1              
 #--------------------------------------------
-
 #Computation for theory
 
 def factorial(num):   
@@ -49,14 +42,12 @@ def factorial(num):
 
 p_ = factorial(365)/((365**NUM_OF_STUDENTS)*factorial(365-NUM_OF_STUDENTS))      
 p = round((1 - p_) *100,5)
-
 #-------------------------------------------------
 
 Percentage = round(count / len(check) * 100,5)
 print('\n')
 print('Chance of double birthdays sampling it %s times: %%%s' %(NUM_OF_SAMPLES,Percentage))
 print('Theoretical chance: %%%s\n' %p)
-
 
 '''
 #Peering into the results from a single sample:                 
