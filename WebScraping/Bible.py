8087ac6a31a5a6392904cf291b1effe5dd450cfa
tadm123 <dramir19@fau.edu>
2016-11-30 18:10:48 -0500

    Stores bible images

--- WebScraping/Bible.py
+++ WebScraping/Bible.py
@@ -1,17 +1,17 @@
-# saves the image into a DailyBibleVerse folder and also accesses the image on the web
+# Downloads images from http://www.verseoftheday.com/ and saves it into a DailyBibleVerse folder 
 
-import requests, os, bs4, webbrowser
+import requests, os, bs4
 
-os.chdir('c:\\users\\patty\\desktop')
+os.chdir('c:\\users\\patty\\desktop')       #modify location where you want to create the folder
 if not os.path.isdir('DailyBibleVerse'):
     os.makedirs('DailyBibleVerse')
 
-res = requests.get('http://www.verseoftheday.com/')     #Downloading the page into res variable
+res = requests.get('http://www.verseoftheday.com/')     #Downloading the page into the 'res' variable
 res.raise_for_status()
 
 soup= bs4.BeautifulSoup(res.text, "html.parser")
 
-#BibleCaption= soup.select('#featured .bilingual-left')   #searching a verse, just to know.
+#BibleCaption= soup.select('#featured .bilingual-left')   #If you want verse use this
 #text= BibleCaption[0].getText()
 
 BibleElem = soup.select('#tv-image-wrapper img')         #searching the image
@@ -23,9 +23,6 @@ while os.path.exists('c:\\users\\patty\\desktop\\DailyBibleVerse\\verse%s.jpg' %
     
 File = open('c:\\users\\patty\\desktop\\DailyBibleVerse\\' +'verse'+ str(num)+ '.jpg', 'wb')  #this is the name
 #if os.path.isfile('c:\\users\\patty\\desktop\\DailyBibleVerse\\'): how to add a number if file already exists?
-
-
-#webbrowser.open(BibleUrl)
 
 res = requests.get(BibleUrl)
 
