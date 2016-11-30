2a1e4f597936036db16146b5e1ca7aad843f4ff3
tadm123 <dramir19@fau.edu>
2016-11-30 18:22:45 -0500

    Update Caesar_Cypher_Shifting_letters.py

--- Strings/Caesar_Cypher_Shifting_letters.py
+++ Strings/Caesar_Cypher_Shifting_letters.py
@@ -1,28 +1,5 @@
-'''# file one.py
-def func():
-    print("func() in one.py")
-
-print("top-level in one.py")
-
-if __name__ == "__main__":
-    print("one.py is being run directly")
-else:
-    print("one.py is being imported into another module")
-
-# file two.py
-import one
-
-print("top-level in two.py")
-one.func()
-
-if __name__ == "__main__":
-    print("two.py is being run directly")
-else:
-    print("two.py is being imported into another module")'''
-
-#http://stackoverflow.com/questions/419163/what-does-if-name-main-do    
-
-
+# ThinkPython  -  http://greenteapress.com/thinkpython2/html/index.html
+# Caesar Cypher program.
 
 def rotate_letter(letter, n):  #  letter= y, n = 3
     #Rotates a letter by n places
@@ -45,34 +22,9 @@ def rotate_word(word,n):    #word = 'Hello there'
 
 
 
-if __name__ == '__main__':         #This just means that this program is being run directly, it's all our
-                                   #source code right here. It wasn't imported from another file into it.
-    print(rotate_word('cheer', 7))
+if __name__ == '__main__':         
+    print(rotate_word('cheer', 7))     #Samples
     print(rotate_word('melon', -10))
     print(rotate_word('sleep', 9))
 
-    
-# So, code in this if block will only run if that module is the
-# entry point to your program.
 
-    
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
- 
