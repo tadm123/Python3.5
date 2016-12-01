1b7daa75bd71ddc1dc9e1a2fd9a9b1119baf32ce
tadm123 <dramir19@fau.edu>
2016-11-30 21:20:42 -0500

    Update adding_compressed_files_into_zip.py

--- Organazing_Files/adding_compressed_files_into_zip.py
+++ Organazing_Files/adding_compressed_files_into_zip.py
@@ -5,16 +5,8 @@
 import zipfile,os
 
 os.chdir('c:\\users\\patty\\desktop')          #Going to desktop
-zp= zipfile.ZipFile('new.zip', 'a')            #using 'a' this time because we're gonna write many files into it
-                                                    #using 'w' would just overwrite existing files with new ones
-#zp.write('spam.txt', compress_type=zipfile.ZIP_DEFLATED)
-#zp.write('Daniel Ramirez Resume.docx', compress_type = zipfile.ZIP_DEFLATED)
-
-
-#note to add folders you gotta use three, to copy what inside of it
-#otherwise it would just copy the folder empty
-# ADDING THE 'My_files' FOLDER:
-
+zp= zipfile.ZipFile('new.zip', 'a')            
+                                                   
 for foldername, subfolders, filenames in os.walk('c:\\users\\patty\\desktop\\My_files'):
     print('Adding current folder : %s' % (foldername))
     zp.write(foldername)
@@ -36,11 +28,6 @@ print('Inside the zip: ')
 for i in zp.namelist():
     print(i)
 
-
-#sum(zp.getinfo(filename).file_size for filename in '\\users')      how do you put a folder (\\users)?
-#print('1st size:')
-#print(sum)
-
 size = sum([zinfo.file_size for zinfo in  zp.filelist])
 size_c = sum([zinfo.compress_size for zinfo in  zp.filelist])
 
@@ -50,15 +37,9 @@ print('Its compressed size: ' + str(size_c))
 zp.close()
 
 
+# Note: It repeats the file because it first copies the 'c:\users\patty\desktop\My_files\Testing' as a subfolder
+# of the current folder you are in (My_files) into the ZIP.
+# In the next iteration when you change the current folders so your root folder is now My_files, it then copies it again
 
-
-
-
-#note it repeats the file because it first copies the 'c:\users\patty\desktop\My_files\Testing' as a subfolder
-#of the current folder you are in (My_files) into the ZIP.
-#In the next iteration when you change the current folders so your root folder is now My_files, it
-#copies it again.
-#This is normal, look at the os_walk()_example.py program that you made. SUBFOLDER is repeated when you
-#go to the next look and look into the current folder is the same.
 
 
