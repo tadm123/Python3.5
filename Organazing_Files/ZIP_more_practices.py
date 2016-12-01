3d73e776bdb03649beb6e2af28b2f13a18c1a7c4
tadm123 <dramir19@fau.edu>
2016-11-30 21:40:51 -0500

    Update ZIP_more_practices.py

--- Organazing_Files/ZIP_more_practices.py
+++ Organazing_Files/ZIP_more_practices.py
@@ -4,7 +4,7 @@
 # certain file extension (such as .pdf or .jpg). Copy these files from whatever location 
 # they are in to a new folder.
 
-'''
+
 #Add only .pdf files from the Desktop into the ZIP file 'PDFfiles.zip'
 
 import zipfile,os
@@ -27,6 +27,7 @@ zp.close()
 
 
 #---------------------------------------------------------------------------------------
+'''
 #Add all files except .txt and .exe from \\loops folder into the ZIP file 'Test.zip'
 
 import zipfile,os
@@ -44,23 +45,7 @@ zp.close()
 
 '''
 # ---------------------------------------------------------------------------------------
-# Find the folder in a directory tree that has the greatest number of files or the folder that
-# uses the most disk space
 
-import os
-
-os.chdir('c:\\users\\patty\\desktop')
-size= 0
-
-for foldername, subfolders, filenames in os.walk('c:\\users\\patty\\desktop\\loops'):
-        print('Current folder: %s' %(foldername))
-   # for subfolders in subfolder
-        for filename in filenames:
-            size += os.path.getsize(os.path.join(foldername,filename))
-        print('Size: %d' %(size))
-        print('Setting size = 0 ')
-        size=0
-    
 
 
 			
